409f2ca154892ccf5a17b119e8eae7ec5daec2f9
omicronrex <65692652+omicronrex@users.noreply.github.com>
2022-02-06 20:31:15 -0300

    add support for older versions of game maker

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,40 +1,76 @@
+#define WNetGetConnectionA WNetGetConnectionA_orig
 #include <windows.h>
+#undef WNetGetConnectionA
+#define DLLEXP extern "C" __declspec(dllexport) __stdcall
 
-extern "C" __declspec(dllexport) __stdcall BOOL SymInitialize(void* a, void* b, void* c) {
+//dbghelp.dll
+DLLEXP BOOL SymInitialize(void* a, void* b, void* c) {
 	return TRUE;
 }
-extern "C" __declspec(dllexport) __stdcall BOOL MiniDumpWriteDump(void* a, void* b, void* c, void* d, void* e, void* f, void* g) {
+DLLEXP BOOL MiniDumpWriteDump(void* a, void* b, void* c, void* d, void* e, void* f, void* g) {
 	return TRUE;
 }
-extern "C" __declspec(dllexport) __stdcall BOOL SymFromAddr(void* a, void* b, void* c, void* d) {
+DLLEXP BOOL SymFromAddr(void* a, void* b, void* c, void* d) {
+	return TRUE;
+}
+DLLEXP BOOL SymGetSymFromAddr(void* a, void* b, void* c, void* d) {
+	return TRUE;
+}
+DLLEXP BOOL SymSetOptions(void* a) {
+	return TRUE;
+}
+DLLEXP DWORD SymGetOptions(void* a) {
+	return 0;
+}
+DLLEXP DWORD WNetGetConnectionA(void* a, void* b, void* c) {
+	return 0;
+}
+DLLEXP DWORD SymLoadModule64(void* a, void* b, void* c, void* d, void* e, void* f) {
+	return 4096;
+}
+DLLEXP BOOL SymGetModuleInfo64(void* a, void* b, void* c) {
+	return TRUE;
+}
+DLLEXP BOOL SymGetLineFromAddr64(void* a, void* b, void* c, void* d) {
+	return TRUE;
+}
+DLLEXP BOOL SymGetSymFromAddr64(void* a, void* b, void* c, void* d) {
 	return TRUE;
 }
 
+
+#define ClosePrinter ClosePrinter_orig
+#undef ClosePrinter
+#define DocumentPropertiesW DocumentPropertiesW_orig
+#undef DocumentPropertiesW
+#define EnumPrintersW EnumPrintersW_orig
+#undef EnumPrintersW
+#define GetDefaultPrinterW GetDefaultPrinterW_orig
+#undef GetDefaultPrinterW
+#define OpenPrinterW OpenPrinterW_orig
+#undef OpenPrinterW
+#define DocumentPropertiesA DocumentPropertiesA_orig
+#undef DocumentPropertiesA
+#define EnumPrintersA EnumPrintersA_orig
+#undef EnumPrintersA
+#define GetDefaultPrinterA GetDefaultPrinterA_orig
+#undef GetDefaultPrinterA
+#define OpenPrinterA OpenPrinterA_orig
+#undef OpenPrinterA
+
+//winspool.drv
+DLLEXP WINBOOL ClosePrinter(HANDLE a) {return TRUE;}
+DLLEXP LONG DocumentPropertiesW(HWND a, HANDLE b, LPWSTR c, PDEVMODEW d, PDEVMODEW e, DWORD f) {return 0;}
+DLLEXP WINBOOL EnumPrintersW(DWORD a, LPWSTR b, DWORD c, LPBYTE d, DWORD e, LPDWORD f, LPDWORD g) {return TRUE;}
+DLLEXP WINBOOL GetDefaultPrinterW(LPWSTR a, LPDWORD b) {return TRUE;}
+DLLEXP WINBOOL OpenPrinterW(LPWSTR a, LPHANDLE b, LPPRINTER_DEFAULTSW c) {return TRUE;}
+DLLEXP LONG DocumentPropertiesA(HWND a, HANDLE b, LPSTR c, PDEVMODEA d, PDEVMODEA e, DWORD f) {return 0;}
+DLLEXP WINBOOL EnumPrintersA(DWORD a, LPSTR b, DWORD c, LPBYTE d, DWORD e, LPDWORD f, LPDWORD g) {return TRUE;}
+DLLEXP WINBOOL GetDefaultPrinterA(LPSTR a, LPDWORD b) {return TRUE;}
+DLLEXP WINBOOL OpenPrinterA(LPSTR a, LPHANDLE b, LPPRINTER_DEFAULTSA c) {return TRUE;}
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
 {
-	switch(fdwReason)
-	{
-		case DLL_PROCESS_ATTACH:
-		{
-			timeBeginPeriod((UINT)1);
-			break;
-		}
-		case DLL_PROCESS_DETACH:
-		{
-			timeEndPeriod((UINT)1);
-			break;
-		}
-		case DLL_THREAD_ATTACH:
-		{
-			break;
-		}
-		case DLL_THREAD_DETACH:
-		{
-			break;
-		}
-	}
-	
-	/* Return TRUE on success, FALSE on failure */
+	timeBeginPeriod(1);
 	return TRUE;
 }

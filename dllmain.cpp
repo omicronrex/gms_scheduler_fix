6e33486e8aed576b4a9a4e928f795f1f9d9dcce3
Lovey847 <lovey847@gmail.com>
2022-05-03 13:35:32 -0500

    Cleanup function stubs

--- dllmain.cpp
+++ dllmain.cpp
@@ -7,51 +7,59 @@
 	__pragma(comment (linker, "/EXPORT:" #_funcname "=_" #_funcname "_overload@" #_argsize)); \
 	extern "C" _type __stdcall _funcname ## _overload
 
+#define DLLEXP4(_type, _funcname) DLLEXP(_type, _funcname, 4)(int)
+#define DLLEXP8(_type, _funcname) DLLEXP(_type, _funcname, 8)(int,int)
+#define DLLEXP12(_type, _funcname) DLLEXP(_type, _funcname, 12)(int,int,int)
+#define DLLEXP16(_type, _funcname) DLLEXP(_type, _funcname, 16)(int,int,int,int)
+#define DLLEXP20(_type, _funcname) DLLEXP(_type, _funcname, 20)(int,int,int,int,int)
+#define DLLEXP24(_type, _funcname) DLLEXP(_type, _funcname, 24)(int,int,int,int,int,int)
+#define DLLEXP28(_type, _funcname) DLLEXP(_type, _funcname, 28)(int,int,int,int,int,int,int)
+
 //dbghelp.dll
-DLLEXP(BOOL, SymInitialize, 12)(void* a, void* b, void* c) {
+DLLEXP12(BOOL, SymInitialize) {
 	return TRUE;
 }
-DLLEXP(BOOL, MiniDumpWriteDump, 28)(void* a, void* b, void* c, void* d, void* e, void* f, void* g) {
+DLLEXP28(BOOL, MiniDumpWriteDump) {
 	return TRUE;
 }
-DLLEXP(BOOL, SymFromAddr, 16)(void* a, void* b, void* c, void* d) {
+DLLEXP16(BOOL, SymFromAddr) {
 	return TRUE;
 }
-DLLEXP(BOOL, SymGetSymFromAddr, 16)(void* a, void* b, void* c, void* d) {
+DLLEXP16(BOOL, SymGetSymFromAddr) {
 	return TRUE;
 }
-DLLEXP(BOOL, SymSetOptions, 4)(void* a) {
+DLLEXP4(BOOL, SymSetOptions) {
 	return TRUE;
 }
-DLLEXP(DWORD, SymGetOptions, 4)(void* a) {
+DLLEXP4(DWORD, SymGetOptions) {
 	return 0;
 }
-DLLEXP(DWORD, WNetGetConnectionA, 12)(void* a, void* b, void* c) {
+DLLEXP12(DWORD, WNetGetConnectionA) {
 	return 0;
 }
-DLLEXP(DWORD, SymLoadModule64, 24)(void* a, void* b, void* c, void* d, void* e, void* f) {
+DLLEXP24(DWORD, SymLoadModule64) {
 	return 4096;
 }
-DLLEXP(BOOL, SymGetModuleInfo64, 12)(void* a, void* b, void* c) {
+DLLEXP12(BOOL, SymGetModuleInfo64) {
 	return TRUE;
 }
-DLLEXP(BOOL, SymGetLineFromAddr64, 16)(void* a, void* b, void* c, void* d) {
+DLLEXP16(BOOL, SymGetLineFromAddr64) {
 	return TRUE;
 }
-DLLEXP(BOOL, SymGetSymFromAddr64, 16)(void* a, void* b, void* c, void* d) {
+DLLEXP16(BOOL, SymGetSymFromAddr64) {
 	return TRUE;
 }
 
 //winspool.drv
-DLLEXP(BOOL, ClosePrinter, 4)(HANDLE a) {return TRUE;}
-DLLEXP(LONG, DocumentPropertiesW, 24)(HWND a, HANDLE b, LPWSTR c, PDEVMODEW d, PDEVMODEW e, DWORD f) {return 0;}
-DLLEXP(BOOL, EnumPrintersW, 28)(DWORD a, LPWSTR b, DWORD c, LPBYTE d, DWORD e, LPDWORD f, LPDWORD g) {return TRUE;}
-DLLEXP(BOOL, GetDefaultPrinterW, 8)(LPWSTR a, LPDWORD b) {return TRUE;}
-DLLEXP(BOOL, OpenPrinterW, 12)(LPWSTR a, LPHANDLE b, LPPRINTER_DEFAULTSW c) {return TRUE;}
-DLLEXP(LONG, DocumentPropertiesA, 24)(HWND a, HANDLE b, LPSTR c, PDEVMODEA d, PDEVMODEA e, DWORD f) {return 0;}
-DLLEXP(BOOL, EnumPrintersA, 28)(DWORD a, LPSTR b, DWORD c, LPBYTE d, DWORD e, LPDWORD f, LPDWORD g) {return TRUE;}
-DLLEXP(BOOL, GetDefaultPrinterA, 8)(LPSTR a, LPDWORD b) {return TRUE;}
-DLLEXP(BOOL, OpenPrinterA, 12)(LPSTR a, LPHANDLE b, LPPRINTER_DEFAULTSA c) {return TRUE;}
+DLLEXP4(BOOL, ClosePrinter) {return TRUE;}
+DLLEXP24(LONG, DocumentPropertiesW) {return 0;}
+DLLEXP28(BOOL, EnumPrintersW) {return TRUE;}
+DLLEXP8(BOOL, GetDefaultPrinterW) {return TRUE;}
+DLLEXP12(BOOL, OpenPrinterW) {return TRUE;}
+DLLEXP24(LONG, DocumentPropertiesA) {return 0;}
+DLLEXP28(BOOL, EnumPrintersA) {return TRUE;}
+DLLEXP8(BOOL, GetDefaultPrinterA) {return TRUE;}
+DLLEXP12(BOOL, OpenPrinterA) {return TRUE;}
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
 {
